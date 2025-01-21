660aa7858acddfef10f76930a23e0633daec3614
Abdullah <162957435+AbdullahNisar123@users.noreply.github.com>
2025-01-21 19:55:02 +0500

    add readme file

--- src/components/OurProducts.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import OurProducts from './OurProducts'
-
-describe('<OurProducts />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<OurProducts />)
-  })
-})
